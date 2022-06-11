d73d575f332ce60c675c70c374b748987fcb88ee
Mayank Srivastava <rsmayank@outlook.com>
2022-06-11 14:39:51 +0530

    Added Radio Buttons on each Contact Card

--- force-app/main/default/lwc/flowRelatedContact/flowRelatedContact.js
+++ force-app/main/default/lwc/flowRelatedContact/flowRelatedContact.js
@@ -1,12 +1,17 @@
 import { LightningElement, track, wire, api} from 'lwc';
 import retrieveContacts from '@salesforce/apex/searchRelatedContacts.retrieveContacts';
 import retrieveContactData from '@salesforce/apex/searchRelatedContacts.retrieveContactData';
+import { FlowAttributeChangeEvent } from 'lightning/flowSupport';
+import StockKeepingUnit from '@salesforce/schema/Product2.StockKeepingUnit';
 
 export default class FlowRelatedContact extends LightningElement {
-    @api recordId ;
-    @track searchVal = '';
+    @api recordId ='0015j000007zxNoAAI';
+    @track searchVal ;
     @track contactData;
     @track errorMsg = '';
+    @api outputTest;
+    @track selectedVal;
+    @track isSelected = false;
     handleChangeAccName(event)
     {
         this.searchVal = event.target.value;    
@@ -22,6 +27,21 @@ export default class FlowRelatedContact extends LightningElement {
         });
 
     }
+
+
+    handleRadio(event)
+    {
+        //No NaN
+        console.log("Helllllooooooo!!!!!!!!");
+        this.selectedVal = event.target.value;
+        console.log(this.selectedVal);
+        this.isSelected = true;
+
+        this.dispatchEvent(new FlowAttributeChangeEvent('outputValue', this.selectedVal));
+
+    }
+    // const attributeChangeEvent = new FlowAttributeChangeEvent('outputTest', this.selectedVal);
+    // this.dispatchEvent(attributeChangeEvent); 
 
     handleShowAll()
     {
@@ -39,6 +59,7 @@ export default class FlowRelatedContact extends LightningElement {
         let index = event.currentTarget.dataset.rowIndex;
         let flipElement = this.template.querySelector('[data-id="' + index + '"]');
         flipElement.classList.add('class1');
+       
     }
 
     removeClass(event){
